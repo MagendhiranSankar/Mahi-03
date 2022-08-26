05b690b73e815101c111d59779f6748265e0f864
MagendhiranSankar <magesankar03@gmail.com>
2022-08-26 12:39:01 +0530

    Welcome

---
+++ src/test/java/EmployeeDetails.java
@@ -0,0 +1,17 @@
+
+public class EmployeeDetails {
+
+	
+	private void method1() {
+		// TODO Auto-generated method stub
+
+		
+	
+	}
+	
+	
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+}
