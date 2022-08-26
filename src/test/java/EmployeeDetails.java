c47673b84660edf6ee446d15cf745e38fdbf6baa
MagendhiranSankar <magesankar03@gmail.com>
2022-08-26 15:40:05 +0530

    2nd Push

--- src/test/java/EmployeeDetails.java
+++ src/test/java/EmployeeDetails.java
@@ -14,4 +14,16 @@ public class EmployeeDetails {
 		// TODO Auto-generated method stub
 
 	}
+	private void method3() {
+		// TODO Auto-generated method stub
+
+		
+	
+	}
+	
+	
+	private void method4() {
+		// TODO Auto-generated method stub
+
+	}
 }
